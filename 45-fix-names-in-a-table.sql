e8360cdb7b1f1e7161e499d515eb1a91c83541ec
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-03-25 22:17:09 +0000

    Create 45-fix-names-in-a-table.sql

---
+++ 45-fix-names-in-a-table.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/fix-names-in-a-table
+SELECT
+    user_id,
+    CONCAT(
+        UPPER(SUBSTRING([name], 1, 1)),
+        LOWER(SUBSTRING([name], 2, LEN([name])))
+    ) AS [name]
+FROM Users
+ORDER BY user_id ASC

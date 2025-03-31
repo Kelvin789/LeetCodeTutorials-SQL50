5c95df315e6d5e99129dd96ac5d54bac04ee4877
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-03-31 23:41:32 +0100

    Create 29-customers-who-bought-all-products.sql

---
+++ 29-customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/customers-who-bought-all-products
+SELECT
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(distinct product_key) = (SELECT COUNT(product_key) FROM Product)

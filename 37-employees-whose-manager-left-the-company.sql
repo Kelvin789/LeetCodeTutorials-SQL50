e3f36512d7e5a2d3b383f75c89aab94e0de5fc8c
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-04-10 23:53:49 +0100

    Update 37-employees-whose-manager-left-the-company.sql

--- 37-employees-whose-manager-left-the-company.sql
+++ 37-employees-whose-manager-left-the-company.sql
@@ -1,13 +1,22 @@
 -- https://leetcode.com/problems/employees-whose-manager-left-the-company
-SELECT 
-    Employees.employee_id
+-- Solution 1:
+-- SELECT 
+--     Employees.employee_id
+-- FROM Employees
+-- LEFT JOIN (
+--     SELECT 
+--         employee_id
+--     FROM Employees
+-- ) AS Managers ON Employees.manager_id = Managers.employee_id
+-- WHERE Managers.employee_id IS NULL
+-- AND Employees.manager_id IS NOT NULL
+-- AND Employees.salary < 30000
+-- ORDER BY Employees.employee_id ASC
+
+-- Solution 2:
+SELECT
+    employee_id
 FROM Employees
-LEFT JOIN (
-    SELECT 
-        employee_id
-    FROM Employees
-) AS Managers ON Employees.manager_id = Managers.employee_id
-WHERE Managers.employee_id IS NULL
-AND Employees.manager_id IS NOT NULL
-AND Employees.salary < 30000
-ORDER BY Employees.employee_id ASC
+WHERE salary < 30000
+AND manager_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id ASC

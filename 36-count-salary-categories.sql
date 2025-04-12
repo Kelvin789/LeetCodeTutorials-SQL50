8f665b8c66640562a4a2fefb50823d6e722480ae
Kelvin789 <67801802+Kelvin789@users.noreply.github.com>
2025-04-12 23:54:51 +0100

    Update 36-count-salary-categories.sql

--- 36-count-salary-categories.sql
+++ 36-count-salary-categories.sql
@@ -1,19 +1,22 @@
 -- https://leetcode.com/problems/count-salary-categories
--- Solution 1:
 SELECT
     'Low Salary' AS category,
     COUNT(1) AS accounts_count
 FROM Accounts
 WHERE income < 20000
-UNION
+
+UNION ALL
+
 SELECT
     'Average Salary' AS category,
-    COUNT(1) AS avgerage_salary_count
+    COUNT(1) AS accounts_count
 FROM Accounts
 WHERE income BETWEEN 20000 AND 50000
-UNION
+
+UNION ALL
+
 SELECT
     'High Salary' AS category,
-    COUNT(1) AS high_salary_count
+    COUNT(1) AS accounts_count
 FROM Accounts
 WHERE income > 50000
